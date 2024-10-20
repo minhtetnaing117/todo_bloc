f019e0522cdcfa8faf7d2db439ce0a2bdde2ed89
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-10-20 20:04:30 +0630

    add todo bloc

--- lib/todo_bloc/todo_state.dart
+++ lib/todo_bloc/todo_state.dart
@@ -1,10 +1,52 @@
 part of 'todo_bloc.dart';
 
-sealed class TodoState extends Equatable {
-  const TodoState();
+enum TodoStatus { initial, loading, success, error }
+
+class TodoState extends Equatable {
+  final List<Todo> todos;
+	final TodoStatus status;
+
+	const TodoState({
+		this.todos = const <Todo>[],
+		this.status = TodoStatus.initial
+	});
   
+   TodoState copyWith({
+    TodoStatus? status,
+    List<Todo>? todos,
+  }) {
+    return TodoState(
+			todos: todos ?? this.todos,
+      status: status ?? this.status,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  factory TodoState.fromJson(Map<String, dynamic> json) {
+    try {
+      var listOfTodos = (json['todo'] as List<dynamic>)
+				.map((e) => Todo.fromJson(e as Map<String, dynamic>))
+				.toList();
+
+      return TodoState(
+				todos: listOfTodos,
+				status: TodoStatus.values.firstWhere(
+					(element) => element.name.toString() == json['status']
+				)
+			);
+    } catch (e) {
+			rethrow;
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+		return {
+			'todo': todos,
+			'status': status.name
+		};
+	}
+
+	@override
+  List<Object?> get props => [todos, status];
 }
 
-final class TodoInitial extends TodoState {}
